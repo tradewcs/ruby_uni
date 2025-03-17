d5e8d7f3bdeb3c85008b841fa70d982f2e633cb0
Tradewcs <tradewcs@gmail.com>
2025-03-17 11:30:59 +0200

    add lab2:

--- lab1/lab1.rb
+++ lab1/lab1.rb
@@ -4,77 +4,54 @@ require 'yaml'
 music_collection = {}
 
 def add_song(collection, title, artists, genres)
-  collection[title.to_sym] = {artists: artists, genres: genres}
+	collection[title.to_sym] = {artists: artists, genres: genres}
 end
 
-def edit_song(collection, title, new_artists: nil, new_genres: nil)
-  return if !collection.key?(title.to_sym)
-  
-  if !new_artists.nil?
-    collection[title.to_sym][:artists] = new_artists
-  end
+def edit_song(collection, title, new_artists, new_genres)
+	return if !collection.key?(title.to_sym)
+	
+	if !new_artists.nil?
+		collection[title.to_sym][:artists] = new_artists
+	end
 
-  if !new_genres.nil?
-    collection[title.to_sym][:genres] = new_genres
-  end
+	if !new_genres.nil?
+		collection[title.to_sym][:genres] = new_genres
+	end
 end
 
 def delete_song(collection, title)
-  collection.delete(title.to_sym)
+	collection.delete(title.to_sym)
 end
 
 def search_song(collection, keyword)
-  collection.select { |title, details| title.to_s.include?(keyword) || details[:artists].any? { |artist| artist.include?(keyword) } }
+	collection.select { |title, details| title.to_s.include?(keyword) || details[:artists].any? { |artist| artist.include?(keyword) } }
 end
 
-
 def save_to_json(collection, filename)
-  File.write(filename, JSON.pretty_generate(collection))
+	File.write(filename, JSON.pretty_generate(collection))
 end
 
 def load_from_json(filename)
-  JSON.parse(File.read(filename), symbolize_names: true) rescue {}
+	JSON.parse(File.read(filename), symbolize_names: true) rescue {}
 end
 
 def save_to_yaml(collection, filename)
-  File.write(filename, collection.to_yaml)
+	File.write(filename, collection.to_yaml)
 end
 
 def load_from_yaml(filename)
-  YAML.load_file(filename, symbolize_names: true) rescue {}
+	YAML.load_file(filename, symbolize_names: true) rescue {}
 end
-'''
-add_song(music_collection, "Сподіваюсь, з тобою все гаразд", ["mistmorn"], ["post-punk"])
-add_song(music_collection, "Небо", ["SadSvit"], ["post-punk"])
-add_song(music_collection, "Танці", ["SadSvit", "Sad Novelist"], ["post-punk"])
-add_song(music_collection, "сам собі наказ", ["хейтспіч", "Sad Novelist"], ["post-punk", "punk-rock"])
-add_song(music_collection, "хвора на любов", ["vioria"], ["kitty-rock"])
-add_song(music_collection, "То моє море", ["ВІКА"], ["synthpop"])
-add_song(music_collection, "Black Catcher", ["VK Blanka"], ["pop"])
-add_song(music_collection, "Мовчати", ["Ірина Білик", "Скрябін"], ["pop"])
-add_song(music_collection, "Реквієм", ["postluv", "Sad Novelist", "Відчай"], ["pop"])
-add_song(music_collection, "Десять причин", ["OTOY"], ["rap"])
-add_song(music_collection, "Загублене літо", ["Electrobirds"], ["post-punk"])
-add_song(music_collection, "Файна", ["Міша Правильний"], ["rap"])
-add_song(music_collection, "Де Твоя Лінія", ["Лінія Маннергейма"], ["rock"])
-add_song(music_collection, "30", ["DK Energetyk"], ["post-punk"])
-add_song(music_collection, "Байдужий", ["Luna Rozza", "BaWN"], ["post-punk"])
-add_song(music_collection, "Пластик", ["паліндром"], ["alt-rock", "post-punk"])
-add_song(music_collection, "Тління", ["Sad Novelist"], [])
-'''
-delete_song(music_collection, "Black Catcher")
 
-edit_song(music_collection, "Тління", new_artists: ["Sad Novelist", "SadSvit"], new_genres: ["post-punk"])
+def main
+	# save_to_json(music_collection, "music.json")
+	# save_to_yaml(music_collection, "music.yml")
 
-p search_song(music_collection, "То моє ")
+	music_collection = load_from_json("music.json")
 
-# p music_collection
+	p search_song(music_collection, "То моє ")
 
+        p music_collection 
+end
 
-# save_to_json(music_collection, "music.json")
-# save_to_yaml(music_collection, "music.yml")
-
-music_collection = load_from_json("music.json")
-
-p music_collection
-p search_song(music_collection, "То моє ")
+main if __FILE__ == $0
---
+++ lab2/lab2.rb
@@ -0,0 +1,75 @@
+require 'json'
+require 'yaml'
+require 'tty-prompt'
+
+require_relative '../lab1/lab1'
+
+music_collection = {}
+
+def main
+  # save_to_json(music_collection, "music.json")
+  # save_to_yaml(music_collection, "music.yml")
+
+  music_collection = load_from_yaml("music.yml")
+
+  prompt = TTY::Prompt.new
+
+  while true
+    choice = prompt.select("Choose an option", %w[Add_Song Edit_Song Delete_Song Search_Song Exit])
+
+    case choice
+    when "Add_Song"
+      title = prompt.ask("Enter the song title:")
+      artists_str = prompt.ask("Enter artists (comma separated):")
+      genres_str = prompt.ask("Enter genres (comma separated):")
+
+      if title && !title.strip.empty? && artists_str && !artists_str.strip.empty?
+        artists = artists_str.split(',').map(&:strip)
+        genres = genres_str.split(',').map(&:strip)
+        
+        add_song(music_collection, title, artists, genres)
+      else
+        puts "\nInvalid input: Both title and artists are required."
+      end
+
+    when "Edit_Song"
+      title = prompt.ask("Enter the song title:")
+      artists_str = prompt.ask("Enter artists (comma separated):")
+      genres_str = prompt.ask("Enter genres (comma separated):")
+
+      artists = artists_str.split(',').map(&:strip)
+      genres = genres_str.split(',').map(&:strip)
+
+      edit_song(music_collection, title, artists, genres)
+
+    when "Delete_Song"
+      title = prompt.ask("Enter the song title:")
+
+      if music_collection.has_key?(title.to_sym)
+        delete_song(music_collection, title)
+      else
+        puts "\nSong '#{title}' is not found."
+      end
+
+    when "Search_Song"
+      keyword = prompt.ask("Search:")
+
+      result = search_song(music_collection, keyword)
+
+			result.each do |title, details|
+				puts "\nSong: #{title}"
+				puts "Artists: #{details[:artists].join(', ')}"
+				puts "Genres; #{details[:genres].join(', ')}"
+			end
+
+    when "Exit"
+      break
+    else
+      puts "\nInvalid option."
+    end
+  end
+
+	save_to_yaml(music_collection, "music.yml")
+end
+
+main if __FILE__ == $0
