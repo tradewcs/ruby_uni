ea35a06eb73102edf3ce76daa86b57badc196274
Tradewcs <tradewcs@gmail.com>
2025-03-24 12:19:59 +0200

    add lab3

---
+++ lab3/music_collection.rb
@@ -0,0 +1,79 @@
+require 'json'
+require 'yaml'
+
+class Song
+  attr_accessor :title, :artists, :genres
+
+  def initialize(title, artists, genres)
+    @title = title
+    @artists = artists
+    @genres = genres
+  end
+end
+
+class MusicCollection
+  attr_accessor :songs
+
+  def initialize
+    @songs = []
+  end
+
+  def add_song(title, artists, genres)
+    @songs << Song.new(title, artists, genres)
+  end
+
+  def edit_song(title, new_artists = nil, new_genres = nil)
+    song = @songs.find { |s| s.title == title }
+    return unless song
+
+    song.artists = new_artists if new_artists
+    song.genres = new_genres if new_genres
+  end
+
+  def delete_song(title)
+    @songs.reject! { |s| s.title == title }
+  end
+
+  def search_song(keyword)
+    @songs.select do |song|
+      song.title.downcase.include?(keyword.downcase) ||
+        song.artists.any?{ |artist| artist.downcase.include?(keyword.downcase) }
+    end
+  end
+
+
+  def output_collection(songs = @songs)
+    songs.each do |song|
+      puts "\nSong: #{song.title}"
+      puts "Artists: #{song.artists.join(', ')}"
+      puts "Genres: #{song.genres.join(', ')}"
+    end
+  end
+
+  def load_from_json(filename)
+    data = JSON.parse(File.read(filename)) rescue []
+    @songs = data.map { |song| Song.new(song['title'], song['artists'], song['genres']) }
+  end
+
+  def save_to_yaml(filename)
+    File.write(filename, @songs.to_yaml)
+  end
+
+  def load_from_yaml(filename)
+    data = YAML.load_file(filename) rescue {}
+
+    @songs = data.map do |title, details|
+      Song.new(title, details['artists'], details['genres'])
+    end
+  end
+end
+
+def main
+  music = MusicCollection.new
+  music.load_from_yaml("music.yml")
+  
+  p music.search_song("То моє ")
+  p music.songs
+end
+
+main if __FILE__ == $0
---
+++ lab3/ui.rb
@@ -0,0 +1,88 @@
+require 'tty-prompt'
+require_relative 'music_collection'
+
+class MusicCollectionManager
+  def initialize(file_path = "music.yml")
+    @file_path = file_path
+
+    @music_collection = MusicCollection.new
+    @music_collection.songs = @music_collection.load_from_yaml(file_path)
+
+    @prompt = TTY::Prompt.new
+  end
+
+  def run
+    loop do
+      choice = @prompt.select("Choose an option", %w[Add_Song Edit_Song Delete_Song Search_Song List_All Exit])
+
+      case choice
+      when "Add_Song"
+        add_song_prompt
+      when "Edit_Song"
+        edit_song_prompt
+      when "Delete_Song"
+        delete_song_prompt
+      when "Search_Song"
+        search_song_prompt
+      when "List_All"
+        list_all_songs
+      when "Exit"
+        break
+      else
+        puts "\nInvalid option."
+      end
+    end
+
+    @music_collection.save_to_yaml(@file_path)
+  end
+
+  private
+
+  def add_song_prompt
+    title = @prompt.ask("Enter the song title:")
+    artists_str = @prompt.ask("Enter artists (comma separated):")
+    genres_str = @prompt.ask("Enter genres (comma separated):")
+
+    if title && !title.strip.empty? && artists_str && !artists_str.strip.empty?
+      artists = artists_str.split(',').map(&:strip)
+      genres = genres_str.split(',').map(&:strip)
+
+      @music_collection.add_song(title, artists, genres)
+    else
+      puts "\nInvalid input: Both title and artists are required."
+    end
+  end
+
+  def edit_song_prompt
+    title = @prompt.ask("Enter the song title:")
+    artists_str = @prompt.ask("Enter artists (comma separated):")
+    genres_str = @prompt.ask("Enter genres (comma separated):")
+
+    artists = artists_str.split(',').map(&:strip)
+    genres = genres_str.split(',').map(&:strip)
+
+    @music_collection.edit_song(title, artists, genres)
+  end
+
+  def delete_song_prompt
+    title = @prompt.ask("Enter the song title:")
+
+    @music_collection.delete_song(title)
+  end
+
+  def search_song_prompt
+    keyword = @prompt.ask("Search:")
+    result = @music_collection.search_song(keyword)
+    @music_collection.output_collection(result)
+  end
+
+  def list_all_songs
+    @music_collection.output_collection
+  end
+end
+
+if __FILE__ == $0
+  manager = MusicCollectionManager.new
+  manager.run
+end
+
