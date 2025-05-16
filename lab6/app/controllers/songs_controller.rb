d77bf76161a5015a5685d6a4ed3334cda4f1b5cd
tradewcs <tradewcs@gmail.com>
2025-05-16 10:16:46 +0300

    some fixes

---
+++ lab6/app/controllers/songs_controller.rb
@@ -0,0 +1,55 @@
+class SongsController < ApplicationController
+  def index
+    @songs = Song.all
+  end
+
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  def new
+    @song = Song.new
+    @artists = Artist.all
+    @genres = Genre.all
+  end
+
+  def create
+    @song = Song.new(song_params)
+    if @song.save
+      redirect_to songs_path, notice: "Song was successfully created."
+    else
+      @artists = Artist.all
+      @genres = Genre.all
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+    @artists = Artist.all
+    @genres = Genre.all
+  end
+
+  def update
+    @song = Song.find(params[:id])
+    if @song.update(song_params)
+      redirect_to song_path(@song), notice: "Song was successfully updated."
+    else
+      @artists = Artist.all
+      @genres = Genre.all
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @song = Song.find(params[:id])
+    @song.destroy
+    redirect_to songs_path, notice: "Song was successfully deleted."
+  end
+
+  private
+
+  def song_params
+    params.require(:song).permit(:title, artist_ids: [], genre_ids: [])
+  end
+end
---
+++ lab6/config/importmap.rb
@@ -0,0 +1,5 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application"
+pin "@rails/ujs", to: "@rails--ujs.js" # @7.1.3
+pin "@rails/ujs", to: "@rails--ujs.js" # @7.1.3
